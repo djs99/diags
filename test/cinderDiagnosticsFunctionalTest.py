a1956520d16e944c55684474dc75f26596e3c277
Nitin Patidar <nitin.patidar@hp.com>
2015-07-16 14:42:57 -0600

    added testcase for hp3parclient

--- test/cinderDiagnosticsFunctionalTest.py
+++ test/cinderDiagnosticsFunctionalTest.py
@@ -1,13 +1,13 @@
 
 import unittest
-from tests import cinderDiagnosticsCPGTest, cinderDiagnosticsCredentialsTest
+from tests import cinderDiagnosticsCPGTest, cinderDiagnosticsCredentialsTest,cinderDiagnosticsHp3parclientTest
 
 
 
 alltests = unittest.TestSuite()
 alltests.addTest(unittest.makeSuite(cinderDiagnosticsCPGTest.BadCPGTest))
 alltests.addTest(unittest.makeSuite(cinderDiagnosticsCredentialsTest.BadCredentialsTest))
-
+alltests.addTest(unittest.makeSuite(cinderDiagnosticsHp3parclientTest.BadHP3PARClientTest))
 #bad_cpg_suite = unittest.TestLoader().loadTestsFromTestCase(cinderDiagnosticsCPGTest.BadCPGTest)
 #bad_credential_suite = unittest.TestLoader().loadTestsFromTestCase(cinderDiagnosticsCredentialsTest.BadCredentialsTest)
 unittest.TextTestRunner(verbosity=2).run(alltests)
