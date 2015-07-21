ee49c7cfa6f44cf02811b67ce7e926ed3857d5a9
Nitin Patidar <nitin.patidar@hp.com>
2015-07-21 15:42:37 -0600

    fixed error

--- test/setup.sh
+++ test/setup.sh
@@ -1,7 +1,7 @@
 logfile1="/var/log/cinder-diagnostics/errorlog1.txt"
 logfile2="/var/log/cinder-diagnostics/errorlog2.txt"
 cinder_diagnostics_log="/var/log/cinder-diagnostics"
-monasca_lib_agent="/usr/lib/monasca/agent/conf.d/"
+monasca_lib_agent="/etc  /monasca/agent/conf.d/"
 monasca_agent="/usr/lib/monasca/agent/conf.d/"
 cinder_diagnostics_conf="cinderDiagnostics.conf"
 cinder_diagnostics_yaml="cinderDiagnostics.yaml"
@@ -11,7 +11,7 @@ logstash="logstash-1.5.0/"
  sudo -H pip install hp3parclient
 # Restart volume services
  sudo service cinder-volume restart
-# Restart volume services java
+# install Java
  sudo apt-get --assume-yes  install default-jre
 
 if [ ! -d "$logstash" ]; then
