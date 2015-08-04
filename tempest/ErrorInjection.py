48b231415c3b30dd306b614d63130d0ec21f873d
Nitin Patidar <nitin.patidar@hp.com>
2015-08-04 10:36:27 -0600

    tempest changes

---
+++ tempest/ErrorInjection.py
@@ -0,0 +1,293 @@
+#!/usr/bin/python
+
+import os
+import shutil
+import time
+import sys
+import argparse
+import os.path
+import subprocess
+import threading
+import ConfigParser
+
+
+class CinderErrors(object):
+	'''
+	Inject cinder configuration error
+	'''
+	cinder_config = "/etc/cinder/cinder.conf"
+  
+
+	def stop_cinder_service(self):
+            output= os.popen("sudo service cinder-volume stop").read()
+	    if 'cinder-volume stop/waiting' not in output:
+	        print ('Something Went Wrong, Could not able to stop Cinder-Volume:', output)
+	    
+
+	def start_cinder_service(self):
+           output= os.popen("sudo service cinder-volume start").read()
+	   if 'cinder-volume start/running' not in output:
+	       print ('Something Went Wrong, Could not able to start Cinder-Volume:', output)
+	   
+	
+        def restart_cinder_service(self):
+            output= os.popen("sudo service cinder-volume restart").read()
+	    if 'cinder-volume start/running' not in output:
+	        print ('Something Went Wrong, Could not able to Restart Cinder-Volume:', output)
+	
+        def is_cinder_service_running(self):
+
+		return true;
+  
+  
+        def bad_3par_credential(self, badUserName, badPassword):
+            #print "Injecting bad 3par credential"
+            badCredential={'hp3par_username' : badUserName, 'hp3par_password' : badPassword}
+            self.inject_error_in_cinder_configuration(badCredential,"3PAR-THEVERSE-FC")
+        
+        
+        def bad_3par_iscsi_ips(self, badIscsiIps):
+            #print "Injecting bad 3par credential"
+            hp3parbadIscsiIps={'hp3par_iscsi_ips' : badIscsiIps}
+            self.inject_error_in_cinder_configuration(hp3parbadIscsiIps,"3PAR-THEVERSE")    
+            
+
+        def bad_3par_cpg(self, badCpg):
+            #print "Injecting bad 3par cpg"
+            badCpg={'hp3par_cpg' : badCpg}
+            self.inject_error_in_cinder_configuration(badCpg,"3PAR-THEVERSE-FC")
+            
+        def bad_3par_ws_url(self, badWsUrl):
+            #print "Injecting bad 3par cpg"
+            
+             try :  
+                cmnd = "yes | sudo -H pip  uninstall hp3parclient"
+                os.popen(cmnd)
+                time.sleep(5)
+                self.restart_cinder_service();
+                time.sleep(15)
+                
+            
+            
+             finally: # catch *all* exceptions
+                os.popen("git clone https://github.com/hp-storage/python-3parclient.git")
+                os.popen("sudo mv python-3parclient/hp3parclient/ /usr/local/lib/python2.7/dist-packages/")
+                badWsUrlDic={'hp3par_api_url' : badWsUrl}
+                self.inject_error_in_cinder_configuration(badWsUrlDic,"3PAR-THEVERSE-FC")
+            
+          
+            
+        def missing_package_3parclient(self):
+            self.inject_cinder_missing_package_error('hp3parclient')
+            
+        
+        def inject_cinder_missing_package_error(self, package_name) :
+            try :  
+                cmnd = "yes | sudo -H pip  uninstall " + package_name
+                os.popen(cmnd)
+                time.sleep(5)
+                self.restart_cinder_service();
+                time.sleep(15)
+            
+            except: # catch *all* exceptions
+                          print sys.exc_info()[0]    
+            finally :
+                # Install package again
+                cmnd = "yes | sudo -H pip install " + package_name
+                os.popen(cmnd)
+                time.sleep(5)
+                self.restart_cinder_service();
+        
+        def inject_nova_missing_package_error(self, package_name) :
+            try :  
+                cmnd = "yes | sudo -H pip  uninstall " + package_name
+                os.popen(cmnd)
+                time.sleep(5)
+                self.restart_cinder_service();
+                time.sleep(15)
+            
+            except: # catch *all* exceptions
+                          print sys.exc_info()[0]    
+            finally :
+                # Install package again
+                cmnd = "yes | sudo -H pip install " + package_name
+                os.popen(cmnd)
+                time.sleep(5)
+                self.restart_cinder_service();
+
+   
+        def inject_error_in_cinder_configuration(self , dict, section) :
+
+	   if os.path.isfile(self.cinder_config):
+	
+                backup_cinder_config= self.cinder_config +".origin"
+		os.rename(self.cinder_config , backup_cinder_config)
+                try :
+                  
+                   config = ConfigParser.RawConfigParser(allow_no_value=True)
+                   config.read(backup_cinder_config)
+                   if config.has_section(section):
+                      for key in dict.keys():
+                          config.set(section,key,dict.get(key))
+                   else :
+                      print "Cinder conf does not cotanin the 3PAR configuration "
+                   # Writing our configuration file to 'cinder.conf'
+                   with open(self.cinder_config, 'w') as configfile:
+                        config.write(configfile)
+                   self.restart_cinder_service();
+                   time.sleep(15)
+                except: # catch *all* exceptions
+                          print sys.exc_info()[0]
+                 
+                finally :
+                    
+                    if os.path.isfile(self.cinder_config):
+                       os.remove(self.cinder_config) 
+  	               os.rename(backup_cinder_config , self.cinder_config)    
+  	            if os.path.isfile(backup_cinder_config):
+                       os.rename(backup_cinder_config , self.cinder_config) 
+                    self.restart_cinder_service();
+           else :
+                 print "No file present : " + self.cinder_config
+
+
+
+
+
+#...........................................................Log Capture.............................................................................................................
+
+cinder_log_path = '/var/log/cinder/temp.log'
+# To verify the string we just keeping log in temprary file and checking if error message is logged or not 
+class LogCapture(threading.Thread):
+        
+
+        def __init__(self):
+                threading.Thread.__init__(self)
+        def run(self):
+                log_write_cmd = 'tail -f /var/log/cinder/cinder-volume.log >> /var/log/cinder/temp.log'
+                global proc
+                proc = subprocess.Popen(log_write_cmd, stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell = True)
+        def terminate_thread(self):
+                try:
+                        proc.terminate()
+                except:
+                        raise
+        def cleanup(self):
+            if os.path.isfile(cinder_log_path):
+                os.remove(cinder_log_path)
+
+#...........................................................Log Verify.............................................................................................................
+
+class LogVerify:
+        
+
+        def __init__(self, log):
+                self.log = log
+
+        def check_log_for_bad_3par_credential(self):
+                cmd = "grep 'Forbidden (HTTP 403) 5 - invalid username or password' " +cinder_log_path
+                proc = subprocess.Popen (cmd, stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell = True)
+                line = proc.stdout.readlines()
+                isInjected = False
+                for error in line:
+                        if 'Forbidden (HTTP 403) 5 - invalid username or password' in error:
+                                print "Error bad_3par_credential Injected Successfully "
+                                print  line
+                                isInjected = True
+                                break
+                if not isInjected :
+                  print "Error bad_3par_credential Injected UnSuccessfully "
+                    
+        def check_log_for_bad_3par_cpg(self):
+
+                cmd1 = '''grep "Invalid input received: CPG (badcpg) doesn't exist on array" ''' +cinder_log_path
+                proc = subprocess.Popen (cmd1, stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell = True)
+                line = proc.stdout.readlines()
+                isInjected = False
+                for error in line:
+                        if "Invalid input received: CPG (badcpg) doesn't exist on array" in error:
+                                print "Error bad_3par_cpg Injected Successfully "
+                                print  line
+                                isInjected = True
+                                break
+                if not isInjected :
+                  print "Error bad_3par_cpg Injected UnSuccessfully "              
+
+        def check_log_for_missing_package_3parclient(self):
+            cmnd = '''grep "You must install hp3parclient before using 3PAR drivers" ''' + cinder_log_path
+            proc = subprocess.Popen(cmnd, stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell = True)
+            lines = proc.stdout.readlines()
+            isInjected = False
+            for errLine in lines:
+                if "You must install hp3parclient before using 3PAR drivers" in errLine:
+                    print "Error missing_package_3parclient Injected Successfully "
+                    print errLine
+                    isInjected = True
+                    break
+            if not isInjected :
+                print "Error missing_package_3parclient Injected UnSuccessfully "
+        
+        def check_log_for_bad_3par_iscsi_ips(self):
+            cmnd = '''grep "Invalid input received: At least one valid iSCSI IP address must be set" ''' + cinder_log_path
+            proc = subprocess.Popen(cmnd, stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell = True)
+            lines = proc.stdout.readlines()
+            isInjected = False
+            for errLine in lines:
+                if "Invalid input received: At least one valid iSCSI IP address must be set" in errLine:
+                    print "Error bad_3par_iscsi_ips Injected Successfully "
+                    print errLine
+                    isInjected = True
+                    break
+            if not isInjected :
+                print "Error bad_3par_iscsi_ips Injected UnSuccessfully "
+                 
+
+
+
+
+#....................................Command Line argument......................................................................
+parser = argparse.ArgumentParser(usage="./ErrorInjection.py [options]...\nExecute'\n")
+parser.add_argument("--debug", dest="debug", action="store_true", help="List out given params")
+# Errors
+parser.add_argument("--bad_3par_credential", dest="bad_3par_credential", action="store_true", help="inject 3par bad credential")
+parser.add_argument("--bad_3par_cpg", dest="bad_3par_cpg", action="store_true", help="inject 3par bad cpg")
+parser.add_argument("--missing_package_3parclient", dest="missing_package_3parclient", action="store_true",
+                        help="inject missing package 3parclient")
+parser.add_argument("--bad_3par_iscsi_ips", dest="bad_3par_iscsi_ips", action="store_true",
+                        help="Inject error 3par bad iscsi ips") 
+parser.add_argument("--bad_3par_ws_url", dest="bad_3par_ws_url", action="store_true",
+                        help="Inject error bad 3par ws url")                        
+
+#................................................ Start Injection..................................................................
+log = LogCapture()
+log.start()
+log_verify = LogVerify(1)
+cinder = CinderErrors()
+args = parser.parse_args()
+
+if len(sys.argv) > 1:
+#  error = sys.argv[1]
+  if args.bad_3par_credential :
+     cinder.bad_3par_credential("bad3paradm","bad3pardata")
+     log_verify.check_log_for_bad_3par_credential()
+
+  elif args.bad_3par_cpg :
+     cinder.bad_3par_cpg("badcpg")
+     log_verify.check_log_for_bad_3par_cpg()
+  
+  elif args.missing_package_3parclient :
+     cinder.missing_package_3parclient()
+     log_verify.check_log_for_missing_package_3parclient()   
+  
+  elif args.bad_3par_iscsi_ips :
+     cinder.bad_3par_iscsi_ips("10.250.100.220555555555:3260")
+     log_verify.check_log_for_bad_3par_iscsi_ips()
+  
+  elif args.bad_3par_ws_url :
+     cinder.bad_3par_ws_url("https://theverse.cxo.sdsdsadhp.com:8080/api/v1")
+  else : 
+    print ' Error Injection does not support : ' + sys.argv[1]
+cinder.restart_cinder_service()
+log.terminate_thread()
+log.cleanup()
+#................................................ End Injection..................................................................
---
+++ tempest/test_cinder_diagnostics.py
@@ -0,0 +1,211 @@
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from tempest.api.monitoring import base
+from tempest_lib import exceptions as lib_exc
+from tempest import test
+from tempest_lib.common.utils import data_utils
+from tempest.common import waiters
+import datetime
+import json
+from tempest.common import ssh
+from tempest import config
+import time
+CONF = config.CONF
+
+class MonitoringCinderDiagnosticsTestJSON(base.BaseMonitoringTest):
+    _interface = 'json'
+
+    @classmethod
+    def setUpClass(cls):
+        super(MonitoringCinderDiagnosticsTestJSON, cls).setUpClass()
+
+    #@test.attr(type="gate")
+    def _test_bad_3par_credentials(self):
+        metric_name = "cinderDiagnostics.credentials"
+        error_injection_name ="bad_3par_credential"
+        self._test_cinder_diagnostics(metric_name, error_injection_name);
+        
+    #@test.attr(type="gate")
+    def _test_bad_3par_cpg(self):
+        metric_name = "cinderDiagnostics.CPG"
+        error_injection_name ="bad_3par_cpg"
+        self._test_cinder_diagnostics(metric_name, error_injection_name);
+        
+    #@test.attr(type="gate")
+    def _test_missing_package_3parclient(self):
+        metric_name = "cinderDiagnostics.3par"
+        error_injection_name ="missing_package_3parclient"
+        self._test_cinder_diagnostics(metric_name, error_injection_name);
+        
+    #@test.attr(type="gate")
+    def _test_bad_3par_iscsi_ips(self):
+        metric_name = "cinderDiagnostics.iSCSI"
+        error_injection_name ="bad_3par_iscsi_ips"
+        self._test_cinder_diagnostics(metric_name, error_injection_name);
+    
+    #@test.attr(type="gate")
+    def _test_bad_3par_ws_url(self):
+        metric_name = "cinderDiagnostics.WS"
+        error_injection_name ="bad_3par_ws_url"
+        self._test_cinder_diagnostics(metric_name, error_injection_name);
+    
+    @test.attr(type="gate")
+    def test_missing_package_sg3utils(self):
+       #metric_name = "cinderDiagnostics.WS"
+        #error_injection_name ="bad_3par_ws_url"
+        self.manager = self.os
+        self.adm_manager= self.os_adm
+        self.image = CONF.compute.image_ref
+        self.flavor = CONF.compute.flavor_ref
+        name = data_utils.rand_name("volume")
+        #extra_specs = '{volume_backend_name:3PAR-SLEEPYKITTY}'
+        #self.adm_manager.volume_types_client.create_volume_type(name='3PAR-THEVERSE')
+        
+        volume = self.manager.volumes_client.create_volume(
+            display_name=name, volume_type='3PAR-THEVERSE')
+        self.manager.volumes_client.wait_for_volume_status(volume['id'],
+                                                          'available')
+       
+
+        # Step 2: create vm instance
+        vm_name = data_utils.rand_name("instance")
+      
+        server = self.manager.servers_client.create_server(
+            vm_name, self.image, self.flavor)
+        server_id = server['id']
+        waiters.wait_for_server_status(self.manager.servers_client, server_id,
+                                       'ACTIVE')
+      
+
+        # Step 3: attach volume to vm
+        
+        self.manager.servers_client.attach_volume(server_id,
+                                                  volume['id'],
+                                                  '/dev/vdc')
+        self.manager.volumes_client.wait_for_volume_status(volume['id'],
+                                                           'in-use')
+        # Step 4: delete vm
+      
+        self.manager.servers_client.delete_server(server_id)
+        self.manager.servers_client.wait_for_server_termination(server_id)
+       
+
+        # Step 5: delete volume
+       
+        self.manager.volumes_client.delete_volume(volume['id'])
+        self.manager.volumes_client.wait_for_resource_deletion(volume['id'])
+        
+        
+        
+        #self._test_cinder_diagnostics(metric_name, error_injection_name);
+    
+    
+ 
+    def _test_cinder_diagnostics(self,  metric_name, error_injection_name):
+        
+        
+        m_starttime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        m_starttime = m_starttime.replace(' ', 'T') + 'Z'
+        
+        # Test case to check if new notification is created successfully.
+        notification_name = data_utils.rand_name('notification-')
+        notification_type = 'EMAIL'
+        # Replace below email with valid email address as required.
+        u_address = 'root@localhost'
+        response = self.monitoring_client.create_notification(name=notification_name, type=notification_type, address=u_address)
+        self.assertEqual(notification_name, response['name'])
+        notification_id = response['id']
+        # Delete notification
+        # 
+        
+        #alarm_def_name = metric_name
+        alarm_def_name = data_utils.rand_name('test_monasca_alarm_definition')
+        expression = '%s > 0' % (metric_name)
+        severity = 'HIGH'
+        body = self.monitoring_client.create_alarm_definition(name=alarm_def_name, expression=expression, severity=severity)
+        self.assertEqual(alarm_def_name, body['name'])
+        alarm_def_id = body['id']
+        self.assertEqual(expression, body['expression'])
+        
+        m_statistics = 'count'
+        m_endtime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        m_endtime = m_endtime.replace(' ', 'T') + 'Z'
+        body = self.monitoring_client.metric_statistics(name=metric_name, dimensions='service:cinder',
+                     statistics=m_statistics, end_time=m_endtime, merge_metrics='true')
+        self.assertEqual('200', body.response['status'])
+        response = json.loads(body.data)
+              
+        
+        # Test normal case for successful connection on first try
+        client = ssh.Client(CONF.cinder_diagnostics.cinder_ssh_ip, CONF.cinder_diagnostics.cinder_ssh_user, CONF.cinder_diagnostics.cinder_ssh_password, timeout=100)
+        sshconnection = client._get_ssh_connection(sleep=1)
+        try:
+            # Setup sftp connection and transmit this script
+            sftp = sshconnection.open_sftp()
+            sftp.put("ErrorInjection.py", "ErrorInjection.py")
+            sftp.close()
+        except IndexError:
+            pass
+        client.exec_command("python ErrorInjection.py --"+error_injection_name)
+        
+        isMetricAvailable = True
+        for i in range(0, 30):
+              metricFields = {'name': metric_name}
+              #metricFields['timestamp'] = int((time.time()*1000))
+              body = self.monitoring_client.list_metric(metricFields)
+              self.assertEqual('200', body.response['status'])
+              response = json.loads(body.data)
+              if len(response['elements']) < 1 :
+                   isMetricAvailable = False
+                   
+        
+        isMeasurementsFound = False
+        isAlarmFound = False
+        isStateChangeToAlarm = False     
+       
+        
+        for i in range(0, 30):
+              m_statistics = 'count'
+              m_endtime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+              m_endtime = m_endtime.replace(' ', 'T') + 'Z'
+              body = self.monitoring_client.metric_statistics(name=metric_name, dimensions='service:cinder',
+                     statistics=m_statistics,start_time=m_starttime, end_time=m_endtime, merge_metrics='true')
+              self.assertEqual('200', body.response['status'])
+              response = json.loads(body.data)
+              if len(response['elements']) > 0 :
+                  if len(response['elements'][0]['statistics']) ==  1:
+                    isMeasurementsFound = True ;
+                    break 
+        
+        for i in range(0, 60):
+            body = self.monitoring_client.list_alarms(alarm_definition_id=alarm_def_id)
+            if len(body['elements']) > 0:
+               isAlarmFound = True
+               if body['elements'][0]['state'] == 'ALARM' :
+                  isStateChangeToAlarm = True
+                  break 
+            time.sleep(3) 
+            
+        self.monitoring_client.delete_notification(notification_id)
+        self.monitoring_client.delete_alarm_definition(alarm_def_id)            
+        
+        if not isMetricAvailable :
+             self.fail("No metric found " + metric_name )
+        if not isMeasurementsFound and not isStateChangeToAlarm :
+             self.fail("No measurements found for " + metric_name ) 
+        if not isAlarmFound :
+             self.fail("No alarm found for " + metric_name )
+        if not isStateChangeToAlarm :
+             self.fail("No state change to ALARM  for  metric " + metric_name )
+     
