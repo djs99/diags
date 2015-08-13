c21641a43447b4d7b237217516338bfd750d7511
ariel <ariel.thompson@hp.com>
2015-08-13 11:48:59 -0600

    first pass at testclient

--- cli/wsapi_conf.py
+++ cli/wsapi_conf.py
@@ -6,39 +6,54 @@ Requires the python hp3parclient: sudo pip install hp3parclient
 Assumes the volume_driver is correctly set
 """
 import ConfigParser
-# TODO: import dummyclient
+
+from oslo_utils import importutils
+import testclient
 hp3parclient = importutils.try_import("hp3parclient")
 if hp3parclient:
     from hp3parclient import client as hpclient
     from hp3parclient import exceptions as hpexceptions
 else:
-    # TODO: report missing package & exit
+    raise ImportError('hp3parclient package is not installed')
+
 
 parser = ConfigParser.SafeConfigParser()
 
 
-class WS_Test(object):
+class WSChecker(object):
+    """
+    Tests web service api configurations by section in the cinder.conf file
+    """
     def __init__(self, test=False, conf='/etc/cinder/cinder.conf'):
+        """
+        :param test: use testing client
+        :param conf: location of cinder.conf
+        """
         self.is_test = test
+        self.conf = conf
 
-        parser.read(conf)
-            self.hp3pars = []
-            for section in parser.sections():
-                if parser.has_option(section, 'volume_driver') \
-                        and 'hp_3par' in parser.get(section, 'volume_driver'):
-                    self.hp3pars.append(section)
 
-    def test_all(self):
+        # This should be set based on where SSHClient copies/location of
+        # test version. Read from cli.conf?  cli.conf read here or values
+        # received from caller?
+        parser.read(self.conf)
+        self.hp3pars = []
+        for section in parser.sections():
+            if parser.has_option(section, 'volume_driver') \
+                    and 'hp_3par' in parser.get(section, 'volume_driver'):
+                self.hp3pars.append(section)
+
+    def check_all(self):
         """
         tests configuration settings for all 3par arrays
         :return: a list of dictionaries
         """
         all_tests = []
         for section_name in self.hp3pars:
-            all_tests.append(self.test_section(section_name))
+            all_tests.append(self.check_section(section_name))
         return all_tests
 
-    def test_section(self, section_name):
+    def check_section(self, section_name):
         """
         Runs all WS configuration tests for a section
         If url/credentials are wrong, no further tests run and values are "unknown"
@@ -51,24 +66,28 @@ class WS_Test(object):
                  "cpg": "unknown",
                  "iscsi": "unknown",
                  }
-        client = get_client(section_name)
-        if client:
-            tests["url"] = "pass"
-            if cred_is_valid(section_name, client):
-                tests["credentials"] = "pass"
-                if cpg_is_valid(section_name, client):
-                    tests["cpg"] = "pass"
+        if section_name in self.hp3pars:
+            client = get_client(section_name, self.is_test)
+            if client:
+                tests["url"] = "pass"
+                if cred_is_valid(section_name, client):
+                    tests["credentials"] = "pass"
+                    if cpg_is_valid(section_name, client):
+                        tests["cpg"] = "pass"
+                    else:
+                        tests["cpg"] = "fail"
+                    if iscsi_is_valid(section_name, client):
+                        tests["iscsi"] = "pass"
+                    else:
+                        tests["iscsi"] = "fail"
+                    client.logout()
                 else:
-                    tests["cpg"] = "fail"
-                if iscsi_is_valid(section_name, client):
-                    tests["iscsi"] = "pass"
-                else:
-                    tests["iscsi"] = "fail"
-            else:
-                tests["credentials"] = "fail"
+                    tests["credentials"] = "fail"
 
+            else:
+                tests["url"] = "fail"
         else:
-            tests["url"] = "fail"
+            raise ValueError('%s not found in %s' % (section_name, self.conf))
         return tests
 
 
@@ -80,12 +99,15 @@ def get_client(section_name, test):
     """
     try:
         if test:
-            cl = dummyclient.HP3Parclient(parser.get(section_name, 'hp3par_api_url'))
+            cl = testclient.HP3ParClient(parser.get(section_name,
+                                                     'hp3par_api_url'))
         else:
             cl = hpclient.HP3ParClient(parser.get(section_name,
                                                  'hp3par_api_url'))
+
         return cl
-    except (hpexceptions.UnsupportedVersion, ConfigParser.NoOptionError):
+    except (hpexceptions.UnsupportedVersion, hpexceptions.HTTPBadRequest,
+            ConfigParser.NoOptionError):
         return None
 
 
