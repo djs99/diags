4ab730d3ba5d65b815f9e1f9ffad5609c0cb7976
Nitin Patidar <nitin.patidar@hp.com>
2015-08-18 15:47:18 -0600

    added testcases for nova packages

--- cli/test/test_cinder_diagnostics_cli_tool_ssh_client.py
+++ cli/test/test_cinder_diagnostics_cli_tool_ssh_client.py
@@ -1,10 +1,24 @@
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
 
 from sshClient import Client
 import pkg_checks
 import config as config
+#from tempest import config
 from base import *
 import os
 import time
+#from tempest import test
 
 CONF = config.CONF
 
@@ -17,7 +31,7 @@ class CinderDiagnostics3PARCliSshClientTest(BaseCinderDiagnosticsCliToolTest):
     def setUpClass(cls):
         super(BaseCinderDiagnosticsCliToolTest, cls).setUpClass()
 
-
+    #@test.attr(type="gate")
     def test_ssh_connection(self) :
         """ Test SSH Connection to cinder node """
         client = None
@@ -33,7 +47,7 @@ class CinderDiagnostics3PARCliSshClientTest(BaseCinderDiagnosticsCliToolTest):
         finally:
             if client is not None :
                 client.disconnect()
-
+    #@test.attr(type="gate")
     def test_get_file(self) :
         """ Test SSH Connection to cinder node """
         client = None
@@ -52,10 +66,28 @@ class CinderDiagnostics3PARCliSshClientTest(BaseCinderDiagnosticsCliToolTest):
                 client.disconnect()
             if os.path.isfile(temp_file) is True:
                 os.remove(temp_file)
+    #@test.attr(type="gate")
+    def test_get_unkown_file(self) :
+        """ Test SSH Connection to cinder node """
+        client = None
+        temp_file = temp = "report.%.7f.conf" % time.time()
 
+        try :
+            client = Client(CONF.cinder_hostname, CONF.cinder_ssh_username, CONF.cinder_ssh_password)
+            client.get_file('/etc/cinder/cinder_not_present.conf', temp_file)
+            self.fail()
+
+        except Exception as e:
+            self.assertEqual(False,os.path.isfile(temp_file))
+
+        finally:
+            if client is not None :
+                client.disconnect()
+            if os.path.isfile(temp_file) is True:
+                os.remove(temp_file)
+    #@test.attr(type="gate")
     def test_nova_pkg_checks(self) :
         """ Test nova packages """
-
         try :
             packages = pkg_checks.nova_check(CONF.nova_hostname, CONF.nova_ssh_username, CONF.nova_ssh_password)
             self.assertEqual(2,len(packages))
@@ -63,7 +95,7 @@ class CinderDiagnostics3PARCliSshClientTest(BaseCinderDiagnosticsCliToolTest):
         except Exception as e:
             self.fail(e.message)
 
-
+    #@test.attr(type="gate")
     def test_successful_ssh_connection_with_mock(self) :
         """ Test SSH Connection with mock """
         try :
@@ -77,7 +109,7 @@ class CinderDiagnostics3PARCliSshClientTest(BaseCinderDiagnosticsCliToolTest):
         finally:
             if client is not None :
                 client.disconnect()
-
+    #@test.attr(type="gate")
     def test_failed_ssh_connection_with_mock(self) :
         """ Test SSH Connection with mock """
 
@@ -92,6 +124,18 @@ class CinderDiagnostics3PARCliSshClientTest(BaseCinderDiagnosticsCliToolTest):
         finally:
             if client is not None :
                 client.disconnect()
+    #@test.attr(type="gate")
+    def test_nova_pkg_checks_with_mock(self) :
+        """ Test SSH Connection with mock """
+
+        try :
+            self._mock_exec_command("install ok installed 2.1.0+repack-3")
+            packages = pkg_checks.nova_check('127.0.0.1' , 'mock', 'mock')
+            self.assertEqual(2,len(packages))
+        except Exception as e :
+            self.assertEqual("Connection unSuccessful", e.message)
+
+
 
 
 if __name__ == '__main__':
