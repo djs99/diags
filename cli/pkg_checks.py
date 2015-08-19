5ab879090f8452b0c2af047be61d8a09bc1b97ff
ariel <ariel.thompson@hp.com>
2015-08-19 14:16:54 -0600

    Squashed commit of the following:
    commit e398dee91d185d44a97ac784d6e58bf8d671e9dc
    Author: ariel <ariel.thompson@hp.com>
    Date:   Wed Aug 19 14:12:06 2015 -0600
    
        tuples in, dictionaries out
    
    commit 3ebc5f464e3bea610eb098698eea5934449071b4
    Author: ariel <ariel.thompson@hp.com>
    Date:   Wed Aug 19 13:57:17 2015 -0600
    
        now accepts ('package name', 'minimum version') as an optional parameter

--- cli/pkg_checks.py
+++ cli/pkg_checks.py
@@ -2,22 +2,32 @@ import sshClient
 import re
 
 
-def nova_check(ip, user, pswd):
-    packages = [{'name': 'sysfsutils', 'min_version': '2.1'},
-                {'name': 'sg3-utils', 'min_version': '1.3'},
-    ]
+def nova_check(ip, user, pswd, pkg_info=('default', 'default')):
+    packages = [('sysfsutils',  '2.1'),
+                ('sg3-utils', '1.3'),
+                ]
+    checked = []
     client = sshClient.Client(ip, user, pswd)
-    for pkg in packages:
-        response = client.execute("dpkg-query -W -f='${Status} ${Version}' " +
-                                  pkg['name'])
-        if 'install ok installed' in response:
-            pkg['installed'] = 'pass'
-            version = re.search('([\d\.]+)', response)
-            if version.group(0) >= pkg['min_version']:
-                pkg['version'] = 'pass'
-            else:
-                pkg['version'] = 'fail'
-        else:
-            pkg['installed'] = 'fail'
+    if pkg_info[0] == 'default':
+        for pkg in packages:
+            checked.append(package_check(client, pkg))
+    else:
+        checked.append(package_check(client, pkg_info))
     client.disconnect()
-    return packages
+    return checked
+
+
+def package_check(client, pkg_info):
+    pkg = {'name': pkg_info[0]}
+    response = client.execute("dpkg-query -W -f='${Status} ${Version}' " +
+                              pkg_info[0])
+    if 'install ok installed' in response:
+        pkg['installed'] = 'pass'
+        version = re.search('([\d\.]+)', response)
+        if version.group(0) >= pkg_info[1]:
+            pkg['version'] = 'pass'
+        else:
+            pkg['version'] = 'fail'
+    else:
+            pkg['installed'] = 'fail'
+    return pkg
