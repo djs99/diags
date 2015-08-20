f13020c8370fe09dacc93fe124e211dfe113dcda
ariel <ariel.thompson@hp.com>
2015-08-20 15:17:57 -0600

    new location of cli.conf

---
+++ cli/conf_reader.py
@@ -0,0 +1,61 @@
+"""
+Reads cli.conf & does the things
+"""
+
+import ConfigParser
+import wsapi_conf
+import ssh_client
+
+PREFIX = 'config/'
+SUFFIX = '.tmp'
+parser = ConfigParser.SafeConfigParser()
+
+class Reader(object):
+
+    def __init__(self, is_test=False):
+        self.is_test = is_test
+        self.cinder_nodes = []
+        self.nova_nodes = []
+
+        parser.read('cli.conf')
+        if self.is_test:
+            self.test_parse()
+        else:
+            self.real_parse()
+
+    def test_parse(self):
+        for section_name in list(parser.sections()):
+            if section_name != 'CLI':
+                if parser.get(section_name, 'service').lower() == 'test':
+                    self.cinder_nodes.append(section_name)
+
+    def real_parse(self):
+        for section_name in list(parser.sections()):
+            if section_name != 'CLI':
+                if parser.get(section_name, 'service').lower() == 'cinder':
+                    self.cinder_nodes.append(section_name)
+                elif parser.get(section_name, 'service').lower() == 'nova':
+                    self.nova_nodes.append(section_name)
+
+    def copy_files(self):
+        for node in self.cinder_nodes:
+            client = ssh_client.Client(parser.get(node, 'host_ip'),
+                                       parser.get(node, 'ssh_user'),
+                                       parser.get(node, 'ssh_password')
+                                       )
+            client.get_file(parser.get(node, 'conf_source'), PREFIX + node + SUFFIX)
+            client.disconnect()
+
+    def ws_checks(self, section_name='all'):
+        checks = []
+        for node in self.cinder_nodes:
+            checker = wsapi_conf.WSChecker(PREFIX + node + SUFFIX, node, self.is_test)
+            if section_name == 'all':
+                checks += checker.check_all()
+            else:
+                found = checker.check_section(section_name)
+                if found:
+                    checks.append(found)
+                
+        return checks
+
---
+++ cli/ssh_client.py
@@ -0,0 +1,46 @@
+import paramiko
+
+
+class Client(object):
+
+    def __init__(self, hostName, sshUserName, sshPassword):
+        """ Connect and perform action to remote machine using SSH
+        """
+        try:
+                # Connect to remote host
+                self.client = paramiko.SSHClient()
+                self.client.set_missing_host_key_policy(
+                    paramiko.AutoAddPolicy())
+                self.client.connect(hostName, username=sshUserName,
+                                password=sshPassword)
+
+        except IndexError:
+            pass
+
+    def get_file(self, fromLocation, toLocation):
+        """ perform copy action to remote machine using SSH
+        """
+        try:
+                # Setup sftp connection and transmit this script
+                sftp = self.client.open_sftp()
+                sftp.get(fromLocation, toLocation)
+                sftp.close()
+
+        except IndexError:
+            pass
+
+    def disconnect(self):
+        """ perform copy action to remote machine using SSH
+        """
+        try:
+                self.client.close()
+
+        except IndexError:
+            pass
+
+    def execute(self, command):
+                # Run the transmitted script remotely without args and show its output.
+                # SSHClient.exec_command() returns the tuple (stdin,stdout,stderr)
+                stdout = self.client.exec_command(command)[1]
+                return stdout.read()
+
