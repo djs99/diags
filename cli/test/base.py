a33a2c29e307a754ed3e307121b9701052eab1ab
Nitin Patidar <nitin.patidar@hp.com>
2015-08-20 11:43:45 -0600

    Improved mocking design

--- cli/test/base.py
+++ cli/test/base.py
@@ -2,6 +2,8 @@
 import unittest
 import mock
 import time
+import ConfigParser
+import os
 
 class BaseCinderDiagnosticsCliToolTest(unittest.TestCase):
 
@@ -25,19 +27,31 @@ class BaseCinderDiagnosticsCliToolTest(unittest.TestCase):
              mock_instance.stop()
 
 
-    def _mock_exec_command(self, return_value):
+    def _mock_exec_command(self, dict):
+
+         # dict is the key value pair of the command and its response response
 
          c_mock, aa_mock, client_mock = self._set_ssh_connection_mocks()
          s_mock = self._patch('time.sleep')
 
          c_mock.return_value = client_mock
-         client_mock.exec_command.return_value = [[], client_mock]
 
-         client_mock.read.return_value =  return_value
+         def my_side_effect(*args, **kwargs):
 
+              is_command_found = False
 
+              for key in dict.keys() :
+                 if args[0] == key:
+                     # Assgin return value to command
+                     client_mock.read.return_value =  dict.get(key)
+                     is_command_found = True
 
+              if not is_command_found :
+                   client_mock.read.return_value = 'command not found'
 
+              return [[], client_mock]
+
+         client_mock.exec_command.side_effect = my_side_effect
 
 
 
@@ -61,5 +75,26 @@ class BaseCinderDiagnosticsCliToolTest(unittest.TestCase):
         return m
 
 
+    def _inject_error_in_cinder_configuration(self , dict, section) :
+
+	   if os.path.isfile(self.cinder_config):
+
+                try :
+                   config = ConfigParser.RawConfigParser(allow_no_value=True)
+                   config.read(self.cinder_config)
+                   if config.has_section(section):
+                      for key in dict.keys():
+                          config.set(section,key,dict.get(key))
+                   else :
+                      print "Cinder conf does not cotanin the 3PAR configuration "
+                   # Writing our configuration file to 'cinder.conf'
+                   with open(self.cinder_config, 'w') as configfile:
+                        config.write(configfile)
+                   self.restart_cinder_service();
+                   time.sleep(15)
+                except Exception as e : # catch *all* exceptions
+                    raise e
+           else :
+                 print "No file present : " + self.cinder_config
 
 
--- cli/test/test_cinder_diagnostics_cli_tool_ssh_client.py
+++ cli/test/test_cinder_diagnostics_cli_tool_ssh_client.py
@@ -32,7 +32,7 @@ class CinderDiagnostics3PARCliSshClientTest(BaseCinderDiagnosticsCliToolTest):
         super(BaseCinderDiagnosticsCliToolTest, cls).setUpClass()
 
     #@test.attr(type="gate")
-    def test_ssh_connection(self) :
+    def _test_ssh_connection(self) :
         """ Test SSH Connection to cinder node """
         client = None
 
@@ -48,7 +48,7 @@ class CinderDiagnostics3PARCliSshClientTest(BaseCinderDiagnosticsCliToolTest):
             if client is not None :
                 client.disconnect()
     #@test.attr(type="gate")
-    def test_get_file(self) :
+    def _test_get_file(self) :
         """ Test SSH Connection to cinder node """
         client = None
         temp_file = temp = "report.%.7f.conf" % time.time()
@@ -67,7 +67,7 @@ class CinderDiagnostics3PARCliSshClientTest(BaseCinderDiagnosticsCliToolTest):
             if os.path.isfile(temp_file) is True:
                 os.remove(temp_file)
     #@test.attr(type="gate")
-    def test_get_unkown_file(self) :
+    def _test_get_unkown_file(self) :
         """ Test SSH Connection to cinder node """
         client = None
         temp_file = temp = "report.%.7f.conf" % time.time()
@@ -86,7 +86,7 @@ class CinderDiagnostics3PARCliSshClientTest(BaseCinderDiagnosticsCliToolTest):
             if os.path.isfile(temp_file) is True:
                 os.remove(temp_file)
     #@test.attr(type="gate")
-    def test_nova_pkg_checks(self) :
+    def _test_nova_pkg_checks(self) :
         """ Test nova packages """
         try :
             packages = pkg_checks.nova_check(CONF.nova_hostname, CONF.nova_ssh_username, CONF.nova_ssh_password)
@@ -98,23 +98,34 @@ class CinderDiagnostics3PARCliSshClientTest(BaseCinderDiagnosticsCliToolTest):
     #@test.attr(type="gate")
     def test_successful_ssh_connection_with_mock(self) :
         """ Test SSH Connection with mock """
+
+        command = 'echo hello'
+        response = 'hello'
+        self._mock_exec_command({command :response})
+
+        client = None
         try :
-            self._mock_exec_command("Successful")
             client = Client('127.0.0.1' , 'mock', 'mock')
-            output = client.execute('echo Successful')
-            self.assertEqual(output, "Successful")
+            output = client.execute(command)
+            self.assertEqual(response , output)
         except Exception as e :
-            self.fail("Connection unSuccessful" )
+            self.fail(e.message)
 
         finally:
             if client is not None :
                 client.disconnect()
+
+
     #@test.attr(type="gate")
     def test_failed_ssh_connection_with_mock(self) :
         """ Test SSH Connection with mock """
 
+        command = 'echo hello'
+        response = Exception("Connection unSuccessful")
+        self._mock_exec_command({command :response})
+
+        client = None
         try :
-            self._mock_exec_command(Exception("Connection unSuccessful"))
             client = Client('127.0.0.1' , 'mock', 'mock')
             output = client.execute('echo Successful')
             self.fail("Connection unSuccessful" )
@@ -124,66 +135,56 @@ class CinderDiagnostics3PARCliSshClientTest(BaseCinderDiagnosticsCliToolTest):
         finally:
             if client is not None :
                 client.disconnect()
-    #@test.attr(type="gate")
-    def test_install_nova_pkg_checks_with_mock(self) :
-        """ Test SSH Connection with mock """
-
-        try :
-            self._mock_exec_command("install ok installed 2.1.0+repack-3")
-            responses = pkg_checks.nova_check('127.0.0.1' , 'mock', 'mock')
-            for response_item in responses :
-                 self.assertEqual("pass",response_item.get("installed"))
-                 self.assertEqual("pass",response_item.get("version"))
-        except Exception as e :
-            self.assertEqual("Connection unSuccessful", e.message)
-
-    def test_version_failure_for_install_nova_pkg(self) :
-        """ Test SSH Connection with mock """
-
-        try :
-            self._mock_exec_command("install ok installed 1.1.0+repack-3")
-            responses = pkg_checks.nova_check('127.0.0.1' , 'mock', 'mock')
-            for response_item in responses :
-                 self.assertEqual("pass",response_item.get("installed"))
-                 self.assertEqual("fail",response_item.get("version"))
-        except Exception as e :
-            self.assertEqual("Connection unSuccessful", e.message)
 
 
     #@test.attr(type="gate")
-    def test_uninstall_nova_pkg_checks_with_mock(self) :
+    def test_installed_nova_pkgs(self) :
         """ Test SSH Connection with mock """
+        self._mock_exec_command({"dpkg-query -W -f='${Status} ${Version}' sysfsutils" :'install ok installed 2.1.0+repack-3' ,
+                                 "dpkg-query -W -f='${Status} ${Version}' sg3-utils" :'install ok installed 2.1.0+repack-3'
+                                 })
 
-        try :
-            self._mock_exec_command("dpkg-query: no packages found matching sysutils")
-            responses = pkg_checks.nova_check('127.0.0.1' , 'fake', 'fake')
-            for response_item in responses :
+        responses = pkg_checks.nova_check('127.0.0.1' , 'mock', 'mock')
+        for response_item in responses :
+            self.assertEqual("pass",response_item.get("installed"))
+            self.assertEqual("pass",response_item.get("version"))
+
+
+    #@test.attr(type="gate")
+    def test_uninstalled_nova_pkgs(self) :
+        """ Test SSH Connection with mock """
+        self._mock_exec_command({"dpkg-query -W -f='${Status} ${Version}' sysfsutils" :'dpkg-query: no packages found matching sysutils' ,
+                                 "dpkg-query -W -f='${Status} ${Version}' sg3-utils" :'dpkg-query: no packages found matching sg3-utils'
+                                 })
+        responses = pkg_checks.nova_check('127.0.0.1' , 'fake', 'fake')
+        for response_item in responses :
                  self.assertEqual("fail",response_item.get("installed"))
-        except Exception as e :
-             self.fail(e.message)
-    #@test.attr(type="gate")
-    def test_version_failure_for_install_nova_pkg(self) :
+
+     #@test.attr(type="gate")
+    def test_less_min_version_for_installed_nova_pkg(self) :
         """ Test SSH Connection with mock """
 
-        try :
-            self._mock_exec_command("install ok installed 1.1.0+repack-3")
-            responses = pkg_checks.nova_check('127.0.0.1' , 'mock', 'mock')
-            for response_item in responses :
-                 self.assertEqual("pass",response_item.get("installed"))
+        self._mock_exec_command({"dpkg-query -W -f='${Status} ${Version}' sysfsutils" :'install ok installed 2.0+repack-3' ,
+                                 "dpkg-query -W -f='${Status} ${Version}' sg3-utils" :'install ok installed 1.0+repack-3'
+                                 })
+
+        responses = pkg_checks.nova_check('127.0.0.1' , 'mock', 'mock')
+        for response_item in responses :
                  self.assertEqual("fail",response_item.get("version"))
-        except Exception as e :
-            self.assertEqual("Connection unSuccessful", e.message)
 
     #@test.attr(type="gate")
-    def test_vaque_response_for_install_nova_pkg(self) :
+    def test_empty_response_for_installed_nova_pkg(self) :
         """ Test SSH Connection with mock """
-        try :
-            self._mock_exec_command("vaque response")
-            responses = pkg_checks.nova_check('127.0.0.1' , 'mock', 'mock')
-            for response_item in responses :
+        self._mock_exec_command({"dpkg-query -W -f='${Status} ${Version}' sysfsutils" :'' ,
+                                 "dpkg-query -W -f='${Status} ${Version}' sg3-utils" :''
+                                 })
+
+        responses = pkg_checks.nova_check('127.0.0.1' , 'mock', 'mock')
+        for response_item in responses :
                  self.assertEqual("fail",response_item.get("installed"))
-        except Exception as e :
-            self.assertEqual("Connection unSuccessful", e.message)
+                 self.assertEqual(None,response_item.get("version"))
+
+
 
 if __name__ == '__main__':
     unittest.main()
