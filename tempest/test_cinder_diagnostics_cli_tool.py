cc0b789e21032af718346e0af46ba28f5d512cbe
Nitin Patidar <nitin.patidar@hp.com>
2015-08-21 13:53:11 -0600

    Modified testcases to run with the CLI tool and tempests

--- tempest/test_cinder_diagnostics_cli_tool.py
+++ tempest/test_cinder_diagnostics_cli_tool.py
@@ -3,13 +3,16 @@ import mock
 import time
 import ConfigParser
 import os
-from sshClient import Client
 import pkg_checks
 from tempest.api.volume import base
 from tempest import test
 from tempest import config
+from tempest_lib.cli import output_parser
+from cinderdiags.ssh_client import Client
 
-CONF = config.CONF
+import cinderdiags.main as cli
+import sys
+
 
 class CinderDiagnostics3PARCliToolTest(base.BaseVolumeAdminTest):
 
@@ -115,7 +118,54 @@ class CinderDiagnostics3PARCliToolTest(base.BaseVolumeAdminTest):
                  self.assertEqual(None,response_item.get("version"))
 
 
+    @test.attr(type="gate")
+    def test_diags_cli_check_array_command(self) :
 
+        # Mock permiko ssh client to return cinder file we want
+        self._mock_get_file("cinder.conf")
+
+        # create cinder config,conf file and add 3par ISCSI section
+        dict = { }
+        # 3par ISCSI section
+        section_name, values = self._get_default_3par_iscsi_cinder_conf_section()
+        dict[section_name] = values
+
+        # 3par FC section
+        section_name, values = self._get_default_3par_fc_cinder_conf_section()
+        dict[section_name] = values
+        #Create cinder.conf
+        self._create_config("cinder.conf",  dict)
+
+        output_file  = "output.%.7f.txt" % time.time()
+
+        #open a file to capture the CLI output
+        sys.stdout  = open(output_file, 'w')
+
+        # Execute the CLI commnad
+        arv=['check', 'array']
+        cli_exit_value = cli.main(arv)
+        self.assertEqual(0 , cli_exit_value)
+
+        table_output = self._convert_table_output(output_file)
+
+        for row in table_output :
+            self.assertEqual('CINDER' , row['node'])
+            self.assertEqual('pass' , row['CPG'])
+            self.assertEqual('pass' , row['Credentials'])
+            self.assertEqual('pass' , row['WS API'])
+
+
+        # remove the file
+        if os.path.isfile(output_file) is True:
+                os.remove(output_file)
+
+        if os.path.isfile("cinder.conf") is True:
+                os.remove("cinder.conf")
+
+    def _convert_table_output(self, filename):
+
+        data = open(filename).read()
+        return output_parser.listing(data)
 
 
     def _set_ssh_connection_mocks(self):
@@ -127,29 +177,37 @@ class CinderDiagnostics3PARCliToolTest(base.BaseVolumeAdminTest):
                 self._patch('paramiko.AutoAddPolicy'),
                 client_mock)
 
+    def _mock_get_file(self, config_file):
+         c_mock, aa_mock, client_mock = self._set_ssh_connection_mocks()
+         s_mock = self._patch('time.sleep')
+         c_mock.return_value = client_mock
+
+         client_mock.open_sftp.return_value = client_mock
+
+         def my_side_effect(*args, **kwargs):
+                 #fromLocation =  args[0]
+                 toLocation  =  args[1]
+                 os.popen("sudo cp " + config_file + " " +toLocation)
+
+
+         client_mock.get.side_effect = my_side_effect
 
     def _mock_exec_command(self, dict):
 
          # dict is the key value pair of the command and its response response
-
          c_mock, aa_mock, client_mock = self._set_ssh_connection_mocks()
          s_mock = self._patch('time.sleep')
-
          c_mock.return_value = client_mock
 
          def my_side_effect(*args, **kwargs):
-
               is_command_found = False
-
               for key in dict.keys() :
                  if args[0] == key:
                      # Assgin return value to command
                      client_mock.read.return_value =  dict.get(key)
                      is_command_found = True
-
               if not is_command_found :
                    client_mock.read.return_value = 'command not found'
-
               return [[], client_mock]
 
          client_mock.exec_command.side_effect = my_side_effect
@@ -164,26 +222,54 @@ class CinderDiagnostics3PARCliToolTest(base.BaseVolumeAdminTest):
         return m
 
 
-    def _inject_error_in_cinder_configuration(self , dict, section) :
+    def _get_default_3par_iscsi_cinder_conf_section(self) :
 
-	   if os.path.isfile(self.cinder_config):
+        section_name = '3PAR-SLEEPYKITTY'
+        dict = {  'volume_driver' : 'cinder.volume.drivers.san.hp.hp_3par_iscsi.HP3PARISCSIDriver',
+                  'volume_backend_name' : '3PAR-SLEEPYKITTY',
+                  'num_volume_device_scan_tries' : 10,
+                  'hp3par_api_url' : 'https://sleepykitty.cxo.hp.com:8080/api/v1',
+                  'hp3par_username' : '3paradm',
+                  'hp3par_password' : '3pardata',
+                  'hp3par_debug' : True,
+                  'san_ip': 'sleepykitty.cxo.hp.com',
+                   'san_login' : '3paradm',
+                   'san_password' : '3pardata',
+                   'hp3par_cpg' : 'OpenStackCPG1',
+                   'hp3par_iscsi_ips' : '10.250.100.220:3260',
+                   'hp3par_iscsi_chap_enabled' : 'false' }
+        return section_name , dict
+
+    def _get_default_3par_fc_cinder_conf_section(self) :
+
+        section_name = '3PAR-SLEEPYKITTY-FC'
+        dict = {  'volume_driver' : 'cinder.volume.drivers.san.hp.hp_3par_fc.HP3PARFCDriver',
+                  'volume_backend_name' : '3PAR-SLEEPYKITTY-FC',
+                  'hp3par_api_url' : 'https://sleepykitty.cxo.hp.com:8080/api/v1',
+                  'hp3par_username' : '3paradm',
+                  'hp3par_password' : '3pardata',
+                  'hp3par_debug' : True,
+                  'san_ip': 'sleepykitty.cxo.hp.com',
+                   'san_login' : '3paradm',
+                   'san_password' : '3pardata',
+                   'hp3par_cpg' : 'OpenStackCPG1' }
+
+        return section_name , dict
+
+    def _create_config(self, config_filename, dict) :
 
                 try :
                    config = ConfigParser.RawConfigParser(allow_no_value=True)
-                   config.read(self.cinder_config)
-                   if config.has_section(section):
-                      for key in dict.keys():
-                          config.set(section,key,dict.get(key))
-                   else :
-                      print "Cinder conf does not cotanin the 3PAR configuration "
-                   # Writing our configuration file to 'cinder.conf'
-                   with open(self.cinder_config, 'w') as configfile:
-                        config.write(configfile)
-                   self.restart_cinder_service();
-                   time.sleep(15)
-                except Exception as e : # catch *all* exceptions
+
+                   for section in dict.keys():
+                       config.add_section(section)
+                       section_attributs = dict.get(section)
+                       for key in section_attributs.keys():
+                             config.set(section,key,section_attributs.get(key))
+                   with open(config_filename, 'w') as configfile:
+                       config.write(configfile)
+
+                except Exception as e :
                     raise e
-           else :
-                 print "No file present : " + self.cinder_config
 
 
