bc08922da7f9137cc8024f85cf2632bf919742f1
ariel <ariel.thompson@hp.com>
2015-08-26 13:12:19 -0600

    added pip checks and 'N/A' result for version=None

--- cli/diagsapp/cinderdiags/pkg_checks.py
+++ cli/diagsapp/cinderdiags/pkg_checks.py
@@ -1,20 +1,27 @@
 import re
 
 
-def check_all(client, node, pkg_info=('default', 'default')):
+def check_all(client, node, pkg_info):
     """
     Check for installed packages on the nova node
     :param client: ssh client
     :param pkg_info: tuple of ('package name', 'minimum version')
     :return: list of dictionaries
     """
-    packages = [('sysfsutils',  '2.1'),
+    default_nova = [('sysfsutils',  '2.1'),
                 ('sg3-utils', '1.3'),
                 ]
+    default_cinder = [('hp3parclient', '3.1.1')]
+
     checked = []
-    if pkg_info[0] == 'default':
-        for pkg in packages:
-            checked.append(check_package(client, node, pkg))
+    if pkg_info[1] == 'default':
+        if pkg_info[0] in ['nova', 'test']:
+            for pkg in default_nova:
+                checked.append(check_package(client, node, pkg))
+
+        if pkg_info[0] in ['cinder', 'test']:
+            for pkg in default_cinder:
+                checked.append(pip_check(client, node, pkg))
     else:
         checked.append(check_package(client, node, pkg_info))
     return checked
@@ -31,10 +38,30 @@ def check_package(client, node, pkg_info):
 
     if response and 'install ok installed' in response:
         pkg['installed'] = 'pass'
-        version = re.search('([\d\.]+)', response)
-        if version.group(0) >= pkg_info[1]:
-            pkg['version'] = 'pass'
+        version = re.search('installed ([\d\.]+)', response)
+        if pkg_info[1]:
+            if version.group(1) >= pkg_info[1]:
+                pkg['version'] = 'pass'
+        else:
+            pkg['version'] = 'N/A'
     elif not response:
-        pkg['installed'] = 'unknown'
-        pkg['version'] = 'unknown'
+        pip_check(client, node, pkg_info)
+    return pkg
+
+
+def pip_check(client, node, pkg_info):
+    pkg = {'node': node,
+           'name': pkg_info[0],
+           'installed': 'fail',
+           'version': 'fail'}
+    response = client.execute("pip list | grep " + pkg_info[0])
+
+    if response:
+        pkg['installed'] = 'pass'
+        version = re.search('\(([\d\.]+)\)', response)
+        if pkg_info[1]:
+            if version.group(1) >= pkg_info[1]:
+                pkg['version'] = 'pass'
+        else:
+            pkg['version'] = 'N/A'
     return pkg
