f56076410e64270e203790a14f917aaa5633f6b6
ariel <ariel.thompson@hp.com>
2015-09-03 13:13:07 -0600

    HP_Drivers

--- cli/diagsapp/cinderdiags/constant.py
+++ cli/diagsapp/cinderdiags/constant.py
@@ -1,8 +1,17 @@
 DIRECTORY = 'config/'
 EXTENSION = '.tmp'
 CLI_CONFIG = 'cli.conf'
-NOVA_PACKAGES = [('sysfsutils',  '2.1'),
-                 ('sg3-utils', '1.3'),
-                ]
+NOVA_PACKAGES = [
+    ('sysfsutils',  '2.1'),
+    ('sg3-utils', '1.3'),
+]
 CINDER_PACKAGES = [('hp3parclient', '3.1.1')]
 
+HP_DRIVERS = [
+    ('hp_3par_fc', '2.0.0'),
+    ('hp_3par_iscsi', '2.0.0'),
+    ('hp_lefthand_iscsi', '1.0.0'),
+    ('hpmsa_fc', '1.0'),
+    ('hpmsa_iscsi', '1.0'),
+    ('hp_xp_fc', None),
+]
