b5c63c41a1f569cfe04464b35d183a2a7f3df64d
Ariel <ariel.thompson@hp.com>
2015-10-01 11:01:52 -0600

    Adding documentation/comments for posterity

--- monasca_agent/cinderDiagnostics.py
+++ monasca_agent/cinderDiagnostics.py
@@ -3,13 +3,6 @@ import time
 import re
 import logging
 from monasca_agent.collector.checks import AgentCheck
-# just so I can test my library, I'm changing the sys.path because otherwise
-#  it can't find anything.  I have absolutely no idea where and how the
-# library needs to be installed to make this work correctly or if that's
-# even possible or if everything is always broken forever and we will all
-# die alone and santa isn't real
-import sys
-sys.path.append('/usr/local/lib/python2.7/dist-packages/')
 from cinder_diagnostics import config_tester
 
 log = logging.getLogger(__name__)
@@ -20,8 +13,6 @@ class Diagnostics(AgentCheck):
     def check(self, instance):
         """
         This check reads JSON text outputted by logstash. It uses
-        cinder_diagnostics.tester to discover which volume backends are
-        mis-configured.
         For reliability it reads 2 files at a staggered interval
         """
 
@@ -32,15 +23,13 @@ class Diagnostics(AgentCheck):
         error_dict.update(self.read_file(instance.get('logpath2')))
 
         for uuid in error_dict:
-            error_dict[uuid]['arrays'] = self.get_arrays(error_dict[uuid][
-                                                             'error_type'])
             # strip all dimensions of forbidden characters
             for dim in error_dict[uuid]:
                 error_dict[uuid][dim] = \
                     re.sub(r'[><=()\'\\;&\{\}\",\^]', '',
                            error_dict[uuid][dim].replace(' ', '_'))
             dimensions = self._set_dimensions(error_dict[uuid], instance)
-            self.increment('08/11.test01.' + error_dict[uuid][
+            self.increment('CinderDiagnostics.' + error_dict[uuid][
                 'error_type'], dimensions=dimensions)
 
     @staticmethod
@@ -54,8 +43,6 @@ class Diagnostics(AgentCheck):
             for line in iter(f):
                 try:
                     data = json.loads(line)
-                    # data = json.loads(line.replace(" ", "_"))
-                    #  these fields must be created by Logstash
                     dims = {'service': data['type'],
                             'hostname': data['host'],
                             'error': data['log_message'],
@@ -78,16 +65,3 @@ class Diagnostics(AgentCheck):
                 ' and permissions allow for read & write.  '
                 'You may need to restart Logstash.' % path)
         return error_dict
-
-    @staticmethod
-    def get_arrays(error_type):
-        if error_type == 'WS':
-            return config_tester.bad_url_list()
-        elif error_type == 'credentials':
-            return config_tester.bad_cred_list()
-        elif error_type == 'CPG':
-            return config_tester.bad_cpg_list()
-        elif error_type == 'iSCSI':
-            return config_tester.bad_iscsi_list()
-        else:
-            return 'unknown'
